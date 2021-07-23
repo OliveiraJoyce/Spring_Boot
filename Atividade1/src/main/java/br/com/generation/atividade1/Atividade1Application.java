ce9e90e8d5569581ddcccec022c9cfb0d1951daf
Joyce Oliveira <86725708+OliveiraJoyce@users.noreply.github.com>
2021-07-23 15:52:31 -0300

    Pastas Spring Boot + atividades

---
+++ Atividade1/src/main/java/br/com/generation/atividade1/Atividade1Application.java
@@ -0,0 +1,13 @@
+package br.com.generation.atividade1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividade1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividade1Application.class, args);
+	}
+
+}
---
+++ Atividade1/src/test/java/br/com/generation/atividade1/Atividade1ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.generation.atividade1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Atividade1ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
